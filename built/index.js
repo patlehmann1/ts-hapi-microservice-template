8104e2595a2742d7ffc70a47be091fb6f59a7824
Pat Lehmann <patrick.lehmann@homes.com>
2021-04-14 20:35:38 -0400

    started a server with hapi

---
+++ built/index.js
@@ -0,0 +1,23 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const Hapi = require('@hapi/hapi');
+const init = () => __awaiter(this, void 0, void 0, function* () {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+    yield server.start();
+    console.log('Server running on %s', server.info.uri);
+});
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+module.exports = () => init();
